ac3eb8c92a63911a142a6f77020d739e29956966
alhambra12 <a221218948@unison.mx>
2025-05-08 09:47:57 -0700

    modificacion de argumentos

--- frontend/app.py
+++ frontend/app.py
@@ -6,14 +6,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir_json', type=str, help='Directorio de JSONs')
-parser.add_argument('--unison_json', type=str, help='Archivo de json con los datos de la universidad')
-parser.add_argument('--scimago_json', type=str, help='Archivo de json con los datos de scimagojr')
+parser.add_argument('--json_dir_path', type=str, help='Directorio de JSONs')
+parser.add_argument('--unison_json_filename', type=str, help='Archivo de json con los datos de la universidad')
+parser.add_argument('--scimago_json_filename', type=str, help='Archivo de json con los datos de scimagojr')
 args = parser.parse_args()
 
-dir_json = args.dir_json or os.path.join(os.path.dirname(__file__), '..', 'datos', 'json')
-unison_json = args.unison_json or 'revistas_unison_test.json'
-scimago_json = args.scimago_json or 'revistas_scimagojr_test.json'
+json_dir_path = args.json_dir_path or os.path.join(os.path.dirname(__file__), '..', 'datos', 'json')
+unison_json_filename = args.unison_json_filename or 'revistas_unison_test.json'
+scimago_json_filename = args.scimago_json_filename or 'revistas_scimagojr_test.json'
+
+json_dir_path = os.path.normpath(json_dir_path)
+unison_json_path = os.path.join(json_dir_path, unison_json_filename)
+scimago_json = os.path.join(json_dir_path, scimago_json_filename)
+
+
 
 @app.route('/')
 def index():
