9f5d2cdb86e923041476a0f91a5f08dd7050b9ed
alhambra12 <a221218948@unison.mx>
2025-05-08 12:36:09 -0700

    función para cargar json de revistas

--- frontend/functions.py
+++ frontend/functions.py
@@ -1,6 +1,30 @@
 import json
+from journal_class import Journal
 
 def load_json(path: str) -> dict:
     ''' Carga un archivo json '''
     with open(path, 'r', encoding='utf8') as f:
         return json.load(f)
+    
+def load_journals(path: str) -> list:
+    ''' Crea una lista con la clase Journal '''
+    journal_json = load_json(path)
+
+    journals = []
+    for idx, (title, info) in enumerate(sorted(journal_json.items())):
+        journal = Journal(
+            id=str(idx),
+            title=title,
+            areas=info.get('areas', []),
+            catalogs=info.get('catalogs', []),
+            website=info.get('website', ''),
+            h_index=info.get('h_index', ''),
+            subjet_area_and_category=info.get('subjet_area_and_category', []),
+            publisher=info.get('publisher', ''),
+            issn=info.get('issn', ''),
+            widget=info.get('widget', ''),
+            publication_type=info.get('publication_type', '')
+        )
+        journals.append(journal)
+
+    return journals
