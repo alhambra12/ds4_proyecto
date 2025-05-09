22340919eb6efff10aaa10fd500b977133a0967e
alhambra12 <a221218948@unison.mx>
2025-05-09 08:37:35 -0700

    funcion para guardar json

--- frontend/journal_json.py
+++ frontend/journal_json.py
@@ -6,3 +6,8 @@ def load_json(path: str) -> dict:
     ''' Carga un archivo json '''
     with open(path, 'r', encoding='utf8') as f:
         return json.load(f)
+    
+def save_json(data: dict, path: str):
+    ''' Guarda diccionario como archivo json '''
+    with open(path, 'w', encoding='utf8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
