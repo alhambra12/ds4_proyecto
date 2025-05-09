4167ec4d19f82c4c1f1d00d934193b3f567d8c4a
alestamalitos <alex_LS04@hotmail.com>
2025-05-09 13:52:44 -0700

    Modificacion functions.py para eligir el resultado mas similar

--- scrapper/functions.py
+++ scrapper/functions.py
@@ -1,10 +1,11 @@
-import json, requests
+import json, requests, Levenshtein
 from bs4 import BeautifulSoup
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' 
      }
 scimagojr_search_url = 'https://www.scimagojr.com/journalsearch.php?q='
 scimagojr_search_url = 'https://www.scimagojr.com/journalsearch.php?q='
+scimagojr_url = 'https://www.scimagojr.com'
 
 ''' Archivo de funciones '''
 
@@ -36,12 +37,21 @@ def find_journal_url(title:str) -> str:
     for a in soup.select('a[href^="journalsearch.php?q="]'):
         title_result = a.select_one('span.jrnlname')
         if title_result:
-            results[title_result.text.strip()] = f"{scimagojr_search_url}/{a['href']}"
+            results[title_result.text.strip()] = f"{scimagojr_url}/{a['href']}"
     if not results:
         print(f"X No hay resultados para '{title}'")
         return None
+    
+    # seleccionar el resultado mas similar
+    best_result = max(results.keys(), key=lambda x: Levenshtein.ratio(title.lower(), x.lower()))
+    ratio = Levenshtein.ratio(title.lower(), best_result.lower())
+    if ratio < 0.9:
+        print(f"X No hay resultados similares a '{title}'")
+        return None
+    print(f"URL encontrada: '{results[best_result]}'")
+    return results[best_result]
 
 if __name__ == '__main__':
-    title = 'FactaUniversitatis,Series:MechanicalEngineering'    
+    title = 'Facta Universitatis, Series: Mechanical Engineering'    
     url = find_journal_url(title)
     
