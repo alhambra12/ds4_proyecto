25ef9581652913d55985ec3a8e7f4a5d95004b1d
alhambra12 <a221218948@unison.mx>
2025-05-09 08:53:51 -0700

    creacion de main

--- frontend/app.py
+++ frontend/app.py
@@ -5,23 +5,7 @@ from functions import load_journals
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--json_dir_path', type=str, help='Directorio de JSONs')
-parser.add_argument('--unison_json_filename', type=str, help='Archivo de json con los datos de la universidad')
-parser.add_argument('--scimago_json_filename', type=str, help='Archivo de json con los datos de scimagojr')
-args = parser.parse_args()
-
-json_dir_path = args.json_dir_path or os.path.join(os.path.dirname(__file__), '..', 'datos', 'json')
-unison_json_filename = args.unison_json_filename or 'revistas_unison_test.json'
-scimago_json_filename = args.scimago_json_filename or 'revistas_scimagojr_test.json'
-
-json_dir_path = os.path.normpath(json_dir_path)
-unison_json_path = os.path.join(json_dir_path, unison_json_filename)
-scimago_json = os.path.join(json_dir_path, scimago_json_filename)
-
-journals_json = 'journals.json'
-journals = load_journals(os.path.join(json_dir_path, journals_json))
+journals = []
 
 @app.route('/')
 def index():
@@ -51,5 +35,26 @@ def journals_catalog(catalogs):
     journals_by_catalog = [j for j in journals if catalogs in j.catalogs]
     return render_template('option_results.html', journals=journals_by_catalog, option=catalogs, option_type='areas')
 
+def main(json_dir_path, unison_json_path, scimago_json_path):
+    global journals
+    journals_json = 'revista.json'
+    journals = load_journals(os.path.join(json_dir_path, journals_json))
+
+    app.run(debug=True) 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json_dir_path', type=str, help='Directorio de JSONs')
+    parser.add_argument('--unison_json_filename', type=str, help='Archivo de json con los datos de la universidad')
+    parser.add_argument('--scimago_json_filename', type=str, help='Archivo de json con los datos de scimagojr')
+    args = parser.parse_args()
+
+    json_dir_path = args.json_dir_path or os.path.join(os.path.dirname(__file__), '..', 'datos', 'json')
+    unison_json_filename = args.unison_json_filename or 'revistas_unison_test.json'
+    scimago_json_filename = args.scimago_json_filename or 'revistas_scimagojr_test.json'
+
+    json_dir_path = os.path.normpath(json_dir_path)
+    unison_json_path = os.path.join(json_dir_path, unison_json_filename)
+    scimago_json_path = os.path.join(json_dir_path, scimago_json_filename)
+
+    main(json_dir_path, unison_json_path, scimago_json_path)
