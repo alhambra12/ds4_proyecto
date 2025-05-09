86ff5af0019cd6765dce97e38cffd48deadf946f
alestamalitos <alex_LS04@hotmail.com>
2025-05-08 22:07:25 -0700

    Funcion scrap en functions.py

--- scrapper/functions.py
+++ scrapper/functions.py
@@ -1,7 +1,17 @@
-import json
+import json, requests
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' 
+     }
 
 ''' Archivo de funciones '''
 
 def load_json(path: str) -> dict:
     with open(path, 'r', encoding='utf8') as f:
         return json.load(f)
+    
+def scrap(url) -> requests.Response:
+    ''' Función para obtener la pagina web desde internet '''
+    page = requests.get(url, headers=headers, timeout=15)
+    if page.status_code != 200:
+        raise Exception(f"X Error: {page.status_code} en '{url}'")
+    return page
