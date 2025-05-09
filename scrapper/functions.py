e771a24073a226ed84060e85c607840297408af9
alestamalitos <alex_LS04@hotmail.com>
2025-05-08 22:50:23 -0700

    Creación de función para encontrar url

--- scrapper/functions.py
+++ scrapper/functions.py
@@ -1,7 +1,10 @@
 import json, requests
+from bs4 import BeautifulSoup
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' 
      }
+scimagojr_search_url = 'https://www.scimagojr.com/journalsearch.php?q='
+scimagojr_search_url = 'https://www.scimagojr.com/journalsearch.php?q='
 
 ''' Archivo de funciones '''
 
@@ -15,6 +18,18 @@ def scrap(url) -> requests.Response:
     if page.status_code != 200:
         raise Exception(f"X Error: {page.status_code} en '{url}'")
     return page
+
+def find_journal_url(title:str) -> str:
+    ''' Función que devuelve la url de una revista a partir de su nombre '''
+    
+    # busca la revista en scimagojr
+    journal_search_url = f"{scimagojr_search_url}{title.replace(' ', '+')}"
+    try:
+        search_page = scrap(journal_search_url)
+    except Exception as e:
+        print(f"X Error buscando '{title}': {e}")
+        return None
+    soup = BeautifulSoup(search_page.text, 'html.parser')
 
 if __name__ == '__main__':
     try:
